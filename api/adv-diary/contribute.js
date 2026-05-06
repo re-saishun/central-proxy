5a94ec1355933d3fc39e0fd0d16725d465bc9ad0
zakaichi.main <zakaichi.main@gmail.com>
2026-05-06 04:09:41 +0000

    Fix: 404 error and CORS issue

--- api/adv-diary/contribute.js
+++ api/adv-diary/contribute.js
@@ -1,16 +1,6 @@
-
 const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
-    // Set CORS headers for all responses
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-    if (req.method === 'OPTIONS') {
-        return res.status(200).end();
-    }
-
     if (req.method !== 'POST') {
         return res.status(405).json({ message: 'Method Not Allowed' });
     }
